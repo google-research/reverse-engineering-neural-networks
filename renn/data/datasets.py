07225c4a236d52bf4928e6ca5b77616d4b52e1d6
Niru Maheswaranathan <niru@fastmail.com>
2020-08-07 11:17:55 -0700

    Adds scripts to preprocess and load the goemotions dataset.

--- renn/data/datasets.py
+++ renn/data/datasets.py
@@ -8,7 +8,7 @@ from renn import utils
 from renn.data.tokenizers import load_tokenizer
 
 
-__all__ = ['imdb', 'tokenize_fun']
+__all__ = ['imdb', 'goemotions', 'tokenize_fun']
 
 
 def pipeline(dset, preprocess_fun=utils.identity, bufsize=1024):
@@ -29,7 +29,44 @@ def tokenize_fun(tokenizer):
   return utils.compose(tokenizer.tokenize, wsp.tokenize, text.case_fold_utf8)
 
 
+def goemotions(split, vocab_file, sequence_length=50, batch_size=64):
+  """Loads the goemotions dataset."""
+  tokenize = tokenize_fun(load_tokenizer(vocab_file))
+  dset = tfds.load('goemotions', split=split)
+
+  emotions = ('admiration', 'amusement', 'anger', 'annoyance', 'approval', 'caring', 'confusion', 'curiosity', 'desire', 'disappointment', 'disapproval', 'disgust', 'embarrassment', 'excitement', 'fear', 'gratitude', 'grief', 'joy', 'love', 'nervousness', 'neutral', 'optimism', 'pride', 'realization', 'relief', 'remorse', 'sadness', 'surprise')
+
+  def _preprocess(d):
+    tokens = tokenize(d['comment_text']).flat_values
+    index = tf.size(tokens)
+    labels = tf.convert_to_tensor([d[e] for e in emotions], dtype=tf.int64)
+    return {
+        'inputs': tokens,
+        'labels': labels,
+        'index': index,
+    }
+
+  # Shapes for the padded batch.
+  padded_shapes = {
+      'inputs': (sequence_length,),
+      'labels': (len(emotions),),
+      'index': (),
+  }
+
+  # Apply dataset pipeline.
+  dset = pipeline(dset, preprocess_fun=_preprocess)
+
+  # Filter out examples longer than the sequence length.
+  dset = dset.filter(lambda d: d['index'] <= sequence_length)
+
+  # Pad remaining examples to the sequence length.
+  dset = dset.padded_batch(batch_size, padded_shapes)
+
+  return dset
+
+
 def imdb(split, vocab_file, sequence_length=1000, batch_size=64):
+  """Loads the imdb reviews dataset."""
   tokenize = tokenize_fun(load_tokenizer(vocab_file))
   dset = tfds.load('imdb_reviews', split=split)
 
--- scripts/generate_vocab.py
+++ scripts/generate_vocab.py
@@ -13,21 +13,40 @@ parser.add_argument('--savedir', type=str, default='/tmp')
 parser.add_argument('--vocab_size', type=int, default=2 ** 15)
 
 
-def imdb_vocab(filename, vocab_size):
+def save_vocab(filename, vocab):
+  with open(filename, 'w') as f:
+    for v in vocab:
+      f.write(v + '\n')
+
+
+def imdb(filename, vocab_size):
   dset = tfds.load('imdb_reviews', split='unsupervised')
 
-  # Build generator that iterates over the corpus.
-  corpus_generator = tfds.as_numpy(dset.map(lambda d: d['text']))
+  # Extract text.
+  dset = tfds.as_numpy(dset.map(lambda d: d['text']))
 
   # Build vocabulary.
-  vocab = renn.data.build_vocab(corpus_generator,
+  vocab = renn.data.build_vocab(iter(dset),
                                 vocab_size,
                                 split_fun=lambda d: d.decode().lower().split())
 
   # Save to file.
-  with open(filename, 'w') as f:
-    for v in vocab:
-      f.write(v + '\n')
+  save_vocab(filename, vocab)
+
+
+def goemotions(filename, vocab_size):
+  dset = tfds.load('goemotions', split='train')
+
+  # Extract text.
+  dset = tfds.as_numpy(dset.map(lambda d: d['comment_text']))
+
+  # Build vocabulary.
+  vocab = renn.data.build_vocab(iter(dset),
+                                vocab_size,
+                                split_fun=lambda d: d.decode().lower().split())
+
+  # Save to file.
+  save_vocab(filename, vocab)
 
 
 def main():
@@ -38,7 +57,10 @@ def main():
   print(f'Saving vocab to: {filename}')
 
   if args.dataset == 'imdb':
-    imdb_vocab(filename, args.vocab_size)
+    imdb(filename, args.vocab_size)
+
+  elif args.dataset == 'goemotions':
+    goemotions(filename, args.vocab_size)
 
   else:
     raise ValueError(f'Invalid dataset {args.dataset}')
