2949ee6ca29a22ec13c39d7154c311abd9062370
ramasesh <vvr1590@gmail.com>
2020-09-06 18:24:15 -0700

    add amazon (#44)
    * add amazon
    
    * change Amazon default
    
    Co-authored-by: Vinay Ramasesh <ramasesh@google.com>

--- renn/data/data_utils.py
+++ renn/data/data_utils.py
@@ -1,5 +1,6 @@
 """ Data utils """
 import csv
+import tensorflow as tf
 
 
 def readfile(filename, parse_row):
@@ -31,6 +32,41 @@ def column_parser(text_column):
     return int(row[0]), row[text_column]
 
   return f
+
+
+def sentiment_relabel(num_classes):
+  """
+  Returns a function which relabels (initially five-class)
+  sentiment labels for subclassing the Yelp and Amazon
+  datasets.
+  """
+
+  LABEL_TYPE = tf.int64
+
+  TENSORS = {
+      -1: tf.constant(-1, dtype=LABEL_TYPE),
+      0: tf.constant(0, dtype=LABEL_TYPE),
+      1: tf.constant(1, dtype=LABEL_TYPE),
+      2: tf.constant(2, dtype=LABEL_TYPE),
+      3: tf.constant(3, dtype=LABEL_TYPE),
+      4: tf.constant(4, dtype=LABEL_TYPE),
+      5: tf.constant(5, dtype=LABEL_TYPE)
+  }
+
+  equal = lambda x, y: tf.cast(tf.equal(x, y), LABEL_TYPE)
+  greater = lambda x, y: tf.cast(tf.greater(x, y), LABEL_TYPE)
+  less = lambda x, y: tf.cast(tf.less(x, y), LABEL_TYPE)
+
+  if num_classes in [1, 2]:
+    return lambda x: greater(x, 3) + equal(x, 3) * TENSORS[-1]
+  elif num_classes == 3:
+    return lambda x: equal(x, 1) * TENSORS[0] + equal(x, 2) * TENSORS[
+        -1] + equal(x, 3) * TENSORS[1] + equal(x, 4) * TENSORS[-1] + equal(
+            x, 5) * TENSORS[2]
+  elif num_classes == 5:
+    return lambda x: tf.subtract(x, 1)
+  else:
+    raise ValueError('Sentiment subclasses must be 1,2,3,5')
 
 
 PARSERS = {
