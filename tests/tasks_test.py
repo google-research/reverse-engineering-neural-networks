de76a8e8875c5447587def72fe96891e772e82b4
Niru Maheswaranathan <nirum@google.com>
2020-11-17 14:29:32 -0800

    Fixes linear regression test.

--- tests/tasks_test.py
+++ tests/tasks_test.py
@@ -21,54 +21,53 @@ import pytest
 from renn.metaopt import tasks
 
 
-class LinearRegressionTest:
+@pytest.mark.parametrize("dim,lambda_min,lambda_max,seed", [
+    (2, -3, 1, 3),
+    (20, -2, 0, 0),
+    (50, -5, -3, 5),
+    (100, 0, 1, 13),
+])
+def test_quadratic(dim, lambda_min, lambda_max, seed):
+  """Smoke test for the quadratic loss function."""
 
-  @pytest.mark.parametrize("dim,num_datapoints,seed,step", [
-      (2, 10, 1, 3),
-      (20, 100, 0, 0),
-      (50, 50, 11, 5),
-      (100, 50, 32, 13),
-  ])
-  def test_loss(self, dim, num_datapoints, seed, step):
-    """Smoke test for the quadratic loss function."""
+  # Build problem.
+  problem_fun = tasks.quad(dim, lambda_min, lambda_max)
+  key = jax.random.PRNGKey(seed)
+  params, loss_fun = problem_fun(key)
 
-    # Build problem.
-    problem_fun = tasks.linreg(dim, num_datapoints, 0.1)
-    key = jax.random.PRNGKey(seed)
-    params, loss_fun = problem_fun(key)
+  loss = loss_fun(params, 0)  # Compute loss.
+  assert loss >= 0.0  # Loss must be non-negative.
 
-    loss = loss_fun(params, step)  # Compute loss.
-    assert loss >= 0.0  # Loss must be non-negative.
 
-  @pytest.mark.parametrize("dim,num_datapoints,num_seeds", [
-      (2, 5, 10),
-      (20, 50, 10),
-  ])
-  def test_batch(self, dim, num_datapoints, num_seeds):
-    """Tests batching of multiple problems."""
+@pytest.mark.parametrize("dim,lambda_min,lambda_max,num_seeds", [
+    (2, -3, 0, 10),
+    (20, 1, 2, 10),
+])
+def test_quadratic_batch(dim, lambda_min, lambda_max, num_seeds):
+  """Tests batching of multiple problems."""
 
-    # Problem builder.
-    problem_fun = tasks.linreg(dim, num_datapoints, 0.1)
+  # Problem builder.
+  problem_fun = tasks.quad(dim, lambda_min, lambda_max)
 
-    # Sample initial parameters.
-    key = jax.random.PRNGKey(0)
-    key, prng = jax.random.split(key)
-    params, _ = problem_fun(prng)
+  # Sample initial parameters.
+  key = jax.random.PRNGKey(0)
+  key, prng = jax.random.split(key)
+  params, _ = problem_fun(prng)
 
-    # Loss to batch over random seeds.
-    def batch_loss(params, prng):
-      _, loss_fun = problem_fun(prng)
-      step = 0  # Loss doesn't depend on the step, so any value here works.
-      return loss_fun(params, step)
+  # Loss to batch over random seeds.
+  def batch_loss(params, prng):
+    _, loss_fun = problem_fun(prng)
+    step = 0  # Loss doesn't depend on the step, so any value here works.
+    return loss_fun(params, step)
 
-    batch_loss = jax.vmap(batch_loss, in_axes=(None, 0))
+  batch_loss = jax.vmap(batch_loss, in_axes=(None, 0))
 
-    # Apply the batched outer loss function.
-    prngs = jax.random.split(key, num_seeds)
-    losses = batch_loss(params, prngs)
+  # Apply the batched outer loss function.
+  prngs = jax.random.split(key, num_seeds)
+  losses = batch_loss(params, prngs)
 
-    # Ensure that all losses are unique.
-    assert len(np.unique(losses)) == num_seeds
+  # Ensure that all losses are unique.
+  assert len(np.unique(losses)) == num_seeds
 
 
 class LogisticRegressionTest:
