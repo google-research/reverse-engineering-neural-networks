22a62baebc5e16d15d9692f5805460bacc107122
Vinay Ramasesh <ramasesh@google.com>
2021-01-12 12:54:05 -0800

    add scaled dot product attention and self-attention

--- renn/rnn/attention.py
+++ renn/rnn/attention.py
@@ -39,3 +39,46 @@ def dot_product_attention(queries: jnp.array,
   attention_weights = jax.nn.softmax(dot_products, axis=-1)
 
   return jnp.dot(attention_weights, values)
+
+def scaled_dot_product_attention(queries: jnp.array,
+                          keys: jnp.array,
+                          values: jnp.array) -> jnp.array:
+  """Computes scaled dot-product attention.
+
+  Arguments:
+    queries: jnp.array, of shape (n_tokens, query_dimension)
+    keys:    jnp.array, of shape (n_tokens, key_dimension)
+    values:  jnp.array, of shape (n_tokens, value_dimension)
+
+  key_dimension must be equal to query_dimension
+  """
+
+  dot_products = jnp.dot(queries, keys.T) #A_ij = q_i . key_j
+  sqrt_d = jnp.sqrt(queries.shape[-1])
+  attention_weights = jax.nn.softmax(dot_products/sqrt_d, axis=-1)
+
+  return jnp.dot(attention_weights, values)
+
+def self_attention(x: jnp.array,
+                   Q: jnp.array,
+                   K: jnp.array,
+                   V: jnp.array) -> jnp.array:
+  """Computes self-attention transformation of x, using
+  Q, K, V as matrices to convert x to queries, keys, and
+  values
+
+  Arguments:
+    x:    jnp.array, of shape (n_tokens, token_dimension)
+    Q:    jnp.array, of shape (query_dimension, token_dimension)
+    K:    jnp.array, of shape (key_dimension, token_dimension)
+    V:    jnp.array, of shape (value_dimension, token_dimension)
+
+  key_dimension must be equal to query_dimension
+  """
+
+  queries = jnp.dot(x, Q.T)
+  keys    = jnp.dot(x, K.T)
+  values  = jnp.dot(x, V.T)
+
+  return scaled_dot_product_attention(queries, keys, values)
+
